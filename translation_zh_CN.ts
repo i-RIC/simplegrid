aa3afa459ea8a48a5ff44219bac97259c2f82e77
zxyecho <58621967+zxyecho@users.noreply.github.com>
2020-02-16 17:22:25 +0900

    Add files via upload

---
+++ translation_zh_CN.ts
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TS version="2.0" language="zh_CN">
+    <!---->
+    <context>
+        <message>
+            <source>Simple Grid Generator</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Simple Channel Shape</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Streamwise Geometry</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Channel Length (m)</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Channel Width (m)</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Channel Slope (m/m)</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Channel Crossing Angle (degrees)</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Number of Meander Wavelengths</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Computational Grid</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Number of stream-wise nodes</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Number of cross-stream nodes</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Cross-sectional Geometry</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Cross-section Type</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Flat</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Parabolic</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Flat Bed Depth (m)</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Parabolic Bank Height (m)</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Parabolic Maimum Depth (m)</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Point Bar Geometry</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Point bar amplitude (0-1)</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Point bar - bend apex phase difference (degrees) (+downstream)</source>
+            <translation></translation>
+        </message>
+        <message>
+            <source>IMax * JMax must be smaller than 100,000.</source>
+            <translation></translation>
+        </message>
+    </context>
+</TS>
