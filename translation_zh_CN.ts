fc87ec21718000e716c30d06825c5a6ef1b51f4c
zxyecho <58621967+zxyecho@users.noreply.github.com>
2020-02-16 17:32:23 +0900

    Update translation_zh_CN.ts

--- translation_zh_CN.ts
+++ translation_zh_CN.ts
@@ -4,91 +4,91 @@
     <context>
         <message>
             <source>Simple Grid Generator</source>
-            <translation></translation>
+            <translation>简单网格生成</translation>
         </message>
         <message>
             <source>Simple Channel Shape</source>
-            <translation></translation>
+            <translation>简单流道形状</translation>
         </message>
         <message>
             <source>Streamwise Geometry</source>
-            <translation></translation>
+            <translation>流向几何</translation>
         </message>
         <message>
             <source>Channel Length (m)</source>
-            <translation></translation>
+            <translation>流道长度(m)</translation>
         </message>
         <message>
             <source>Channel Width (m)</source>
-            <translation></translation>
+            <translation>流道宽度(m)</translation>
         </message>
         <message>
             <source>Channel Slope (m/m)</source>
-            <translation></translation>
+            <translation>流道坡度(m/m)</translation>
         </message>
         <message>
             <source>Channel Crossing Angle (degrees)</source>
-            <translation></translation>
+            <translation>通道交叉角（度）</translation>
         </message>
         <message>
             <source>Number of Meander Wavelengths</source>
-            <translation></translation>
+            <translation>弯曲波长数</translation>
         </message>
         <message>
             <source>Computational Grid</source>
-            <translation></translation>
+            <translation>计算网格</translation>
         </message>
         <message>
             <source>Number of stream-wise nodes</source>
-            <translation></translation>
+            <translation>流向节点数</translation>
         </message>
         <message>
             <source>Number of cross-stream nodes</source>
-            <translation></translation>
+            <translation>跨流节点数</translation>
         </message>
         <message>
             <source>Cross-sectional Geometry</source>
-            <translation></translation>
+            <translation>横截面几何</translation>
         </message>
         <message>
             <source>Cross-section Type</source>
-            <translation></translation>
+            <translation>横截面种类</translation>
         </message>
         <message>
             <source>Flat</source>
-            <translation></translation>
+            <translation>平坦</translation>
         </message>
         <message>
             <source>Parabolic</source>
-            <translation></translation>
+            <translation>抛物线</translation>
         </message>
         <message>
             <source>Flat Bed Depth (m)</source>
-            <translation></translation>
+            <translation>平坦床深度(m)</translation>
         </message>
         <message>
             <source>Parabolic Bank Height (m)</source>
-            <translation></translation>
+            <translation>抛物线河流高(m)</translation>
         </message>
         <message>
             <source>Parabolic Maimum Depth (m)</source>
-            <translation></translation>
+            <translation>抛物线最大深度(m)</translation>
         </message>
         <message>
             <source>Point Bar Geometry</source>
-            <translation></translation>
+            <translation>点栏几何</translation>
         </message>
         <message>
             <source>Point bar amplitude (0-1)</source>
-            <translation></translation>
+            <translation>点栏振幅 (0-1)</translation>
         </message>
         <message>
             <source>Point bar - bend apex phase difference (degrees) (+downstream)</source>
-            <translation></translation>
+            <translation>点条-弯曲顶点相位差（度）（+下游）</translation>
         </message>
         <message>
             <source>IMax * JMax must be smaller than 100,000.</source>
-            <translation></translation>
+            <translation>IMax * JMax必须小于100,000。</translation>
         </message>
     </context>
 </TS>
